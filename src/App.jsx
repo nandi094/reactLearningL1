ec84fc5c56082d3d72b37f7edec6a7ac827e5f18
nandi094 <nandigowda094@gmail.com>
2024-07-25 15:41:13 +0530

    mmmm :wq

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { useState } from "react";
 // import componentsImg from "./assets/components.png";
 import Header from "./components/Header.jsx";
-  import { CORE_CONCEPTS } from "./data.js";
+import { CORE_CONCEPTS } from "./data.js";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
-
+import { EXAMPLES } from "./data.js";
 
 function App() {
-const [ selectedTopic, setSelectedTopic] = useState('Please select a Value');
-  function handleSelect(selectedButton){
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleSelect(selectedButton) {
     // console.log("Hello World! - Selected "+selectedButton);
     setSelectedTopic(selectedButton);
     // console.log(selectedTopic);
-}
+  }
+
+  let tabContent = <p>Please Select a Topic</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
   return (
     <div>
       <Header />
@@ -20,34 +34,21 @@ const [ selectedTopic, setSelectedTopic] = useState('Please select a Value');
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-          <CoreConcept
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-            image={CORE_CONCEPTS[0].image}
-          />
-          <CoreConcept
-            title={CORE_CONCEPTS[1].title}
-            description={CORE_CONCEPTS[1].description}
-            image={CORE_CONCEPTS[1].image}
-          />
-          <CoreConcept
-          {...CORE_CONCEPTS[2]}
-          />
-          <CoreConcept
-          {...CORE_CONCEPTS[3]}
-          />
-          </ul>
+            {CORE_CONCEPTS.map((compItems) => (<CoreConcept key={compItems.title} {...compItems}/>))}
+            </ul>
           <h2>Time to get started!</h2>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-          <TabButton onSelect={() => handleSelect('component')}>Component</TabButton>
-          <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-          <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected = {selectedTopic==='components'} onSelect={() => handleSelect("components")}>
+              Component
+            </TabButton>
+            <TabButton isSelected = {selectedTopic==='jsx'} onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton isSelected = {selectedTopic==='props'} onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton isSelected = {selectedTopic==='state'} onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {tabContent}
         </section>
       </main>
     </div>
